456035ddc77e923496fa589c04827b6e5e8254e1
Pointless <150302154+PointlessCircle@users.noreply.github.com>
2023-11-27 18:04:41 -0500

    Testing generating buttons

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-document.getElementById("test").innerHTML = window.location.href;
+document.getElementById("test").innerHTML = window.location.pathname;
+
+function cB(test, row){
+  var button = document.createElement('button');
+  button.innerHTML = test;
+  button.onclick = testing(test);
+  document.getElementById(row).appendChild(button);
+};
+
+function testing(t2) {
+    console.log(t2);
+}
+
+cB(1,row1);
+cB(2,row1);
+cB(3,row2);
