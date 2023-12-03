25209530c3e642ca6c178c24f1e2ff65357f8627
Pointless <150302154+PointlessCircle@users.noreply.github.com>
2023-12-03 10:03:20 -0500

    Fixed event firing

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ document.getElementById("test").innerHTML = window.location.pathname;
 function cB(test, row){
   var button = document.createElement('button');
   button.innerHTML = test;
-  button.onclick = testing(test);
+  button.onclick = function() {testing(test)};
   document.getElementById(row).appendChild(button);
 };
 
 function testing(t2) {
-    console.log(t2);
+    alert(t2);
 }
 
-cB(1,r1);
-cB(2,r1);
-cB(3,r2);
+cB('1','r1');
+cB('2','r1');
+cB('3','r2');
